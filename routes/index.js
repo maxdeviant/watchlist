10c3625f04e60ca4c967aea81e905adf39267642
Marshall Bowers <elliott.codes@gmail.com>
2014-11-05 13:42:58 -0500

    Add registration screen

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,17 @@ var minifyOptions = {
 var router = express.Router();
 
 router.route('/').get(function (req, res) {
-    res.render('index', {}, function (err, data) {
+    res.render('index', function (err, data) {
         return res.send(minify(data, minifyOptions));
     });
+});
+
+router.route('/register').get(function (req, res) {
+    res.render('register', function (err, data) {
+        return res.send(minify(data, minifyOptions));
+    });
+}).post(function (req, res) {
+    
 });
 
 module.exports = router;
