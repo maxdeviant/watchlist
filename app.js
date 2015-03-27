cd37158a88d6cec38960a8cd0b39b415d792f708
Marshall Bowers <elliott.codes@gmail.com>
2015-03-26 20:28:43 -0400

    Add user registration

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 
+var User = require('./models/user');
+
 mongoose.connect('mongodb://localhost/watchlist');
 
 var app = express();
@@ -30,6 +32,52 @@ router.route('/')
         };
 
         return res.render('index');
+    });
+
+router.route('/register')
+    .get(function (req, res) {
+        app.locals.page = {
+            title: 'Register'
+        };
+
+        return res.render('register');
+    })
+    .post(function (req, res) {
+        User.findOne({
+            username: req.body.username
+        }, function (err, user) {
+            if (err || user !== null) {
+                app.locals.error = 'Username is not available.';
+
+                return res.render('register');
+            }
+
+            var user = new User();
+
+            user.username = req.body.username;
+            user.password = req.body.password;
+            user.firstName = req.body.firstname;
+            user.lastName = req.body.lastname;
+
+            user.save(function (err) {
+                if (err) {
+                    app.locals.error = 'An error occurred during registration.';
+
+                    return res.render('register');
+                }
+
+                return res.redirect('/');
+            });
+        });
+    });
+
+router.route('/login')
+    .get(function (req, res) {
+        app.locals.page = {
+            title: 'Log In'
+        };
+
+        return res.render('login');
     });
 
 app.use('/', router);
---
+++ models/user.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        default: ''
+    },
+    lastName: {
+        type: String,
+        default: ''
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) {
+            return callback(err);
+        }
+
+        callback(null, isMatch);
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+
+            next();
+        });
+    });
+});
+
+module.exports = User;
